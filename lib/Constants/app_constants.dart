e34f71c458ad5768c3a8083394d9fbe1fd687709
Horizon Build <horizon@example.com>
2026-05-14 23:41:00 -0700

    Rename: Update logo filenames from reins to horizon

--- lib/Constants/app_constants.dart
+++ lib/Constants/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   static const String appName = 'Horizon';
-  static const String appIconPng = 'assets/images/reins.png';
-  static const String appIconSvg = 'assets/images/reins.svg';
+  static const String appIconPng = 'assets/images/horizon.png';
+  static const String appIconSvg = 'assets/images/horizon.svg';
 
   static const String ollamaIconPng = 'assets/images/ollama.png';
 }
